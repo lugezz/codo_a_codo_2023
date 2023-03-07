c124a4de8d9cdca6be1dcaa85d15687c035c3e9b
Eugenio <lugezz@gmail.com>
2023-03-07 20:34:25 -0300

    Clase 3 Codo a Codo

---
+++ Clase_03/codigo.js
@@ -0,0 +1,52 @@
+// Interacciones -----------------------
+
+// alert('Hello')
+
+// let nombre = prompt('Ingresa tu nombre:')
+// console.log(nombre)
+
+// confirm('Querés ingresar?')
+
+// Variables
+
+let nombre = "Eugenio"; // string
+let edad = 43; // number
+let estudia = true; // boolean
+let materias = ['react', 'javascript', 'node']; // array
+let finalizado = null; // 
+
+
+console.log('Nombre:', nombre, 'Tipo:', typeof nombre)
+// 'string'
+console.log('Edad:', edad, 'Tipo:', typeof edad)
+// 'number'
+console.log('Estudia:', estudia, 'Tipo:', typeof estudia)
+// 'boolean'
+console.log('Materias:', materias, 'Tipo:', typeof materias)
+// 'object'
+console.log('Finalizado:', finalizado, 'Tipo:', typeof finalizado)
+// 'object'
+
+// Más
+
+let numeroFavorito = "13"
+console.log(numeroFavorito + edad)
+// 1343
+console.log(numeroFavorito * edad)
+// 559
+console.log(parseInt(numeroFavorito) + edad)
+// 56
+
+
+// -------
+
+let num1 = prompt('Ingrese número 1')
+let num2 = prompt('Ingrese número 2')
+
+alert(`La suma de ${num1} más ${num2} da ${parseInt(num1) + parseInt(num2)}`)
+
+
+// let num3 = prompt('Ingrese número 1')
+// let num4 = prompt('Ingrese número 2')
+
+// alert(`El ${num3} elevado a ${num4} da ${num3 ** num4}`)
